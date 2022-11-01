a9ddc998d02dec6ca6745dc37e4844782927934d
Firhan Imam Haekal <45736333+FirH@users.noreply.github.com>
2022-11-01 18:00:45 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,9 +1,10 @@
 import pickle
 import numpy as np
 import pandas as pd
+import streamlit as st
 
-with open ("./prediksi_harga_rumah.pkl", "rb") as f:
-    model = pickle.load(f)
+with open ("prediksi_harga_rumah.pkl", "rb") as f:
+     model = pickle.load(f)
 
 def prediksi(LT,LB,JKT,JKM,GRS):
     predict = pd.DataFrame()
@@ -12,7 +13,12 @@ def prediksi(LT,LB,JKT,JKM,GRS):
     predict['JKT'] = [JKT]
     predict['JKM'] = [JKM]
     predict['GRS'] = [GRS]
-    print(model.predict(predict)[0])
+    return(model.predict(predict)[0])
 
+lt = st.number_input("Input LT")
+lb = st.number_input("Input LB")
+jkt = st.number_input("Input JKT")
+jkm = st.number_input("Input JKM")
+grs = st.number_input("Input GRS")
 
-prediksi(100,100,2,2,1)
+st.write(prediksi(lt,lb,jkt,jkm,grs))
