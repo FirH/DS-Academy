b0eb2709229048afbf94fece3866a451a0b1de97
Firhan Imam Haekal <firhanimamhaekal@gmail.com>
2022-11-05 07:44:32 +0700

    Code Update

--- deploy.py
+++ deploy.py
@@ -1,18 +1,27 @@
 import pickle
 import numpy as np
 import pandas as pd
+import streamlit as st
 
-with open ("./prediksi_harga_rumah.pkl", "rb") as f:
+with open ("prediksi_harga_rumah.pkl", "rb") as f:
     model = pickle.load(f)
 
 def prediksi(LT,LB,JKT,JKM,GRS):
     predict = pd.DataFrame()
-    predict['LT'] = [LT]
-    predict['LB'] = [LB]
-    predict['JKT'] = [JKT]
-    predict['JKM'] = [JKM]
-    predict['GRS'] = [GRS]
-    print(model.predict(predict)[0])
+    predict['Luas Tanah'] = [LT]
+    predict['Luas Bangunan'] = [LB]
+    predict['Jumlah Kamar Tidur'] = [JKT]
+    predict['Jumlah Kamar Mandi'] = [JKM]
+    predict['Garasi'] = [GRS]
+    return(model.predict(predict)[0])   
 
+lt = st.number_input("Input LT")
+lb = st.number_input("Input LB")
+jkt = st.number_input("Input JKT")
+jkm = st.number_input("Input JKM")
+grs = st.number_input("Input GRS")
 
-prediksi(100,100,2,2,1)
+predict_button = st.button('Predict')
+
+if(predict_button) :
+    st.write(prediksi(lt,lb,jkt,jkm,grs))
